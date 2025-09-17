c7621668e46c1a366cadd765c6183dd866c04337
keniofront <keniofront@gmail.com>
2025-09-17 17:02:05 -0300

    fix: configuração do github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// src/App.jsx
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import "./index.css";
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "movies-api-react",
+  base: "/movies-api-react/",
 });
